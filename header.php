e5c44e19a87deeaf61b89d7413476a7c3a3a1af8
Aman672002 <amanbosamiya0@gmail.com>
2025-04-08 17:49:47 -0400

    Update header.php

--- header.php
+++ header.php
@@ -7,10 +7,12 @@
 
 <body>
 <nav>
+	<p>This is changed file</p>
 	<a href="session_example.php">Home</a> | 
 	<a href="session_example.php?action=forget">Forget Me</a> 
 	<?php if(isset($_SESSION['username']) && isset($_SESSION['email'])): ?>
 		| <a href="session_example.php?action=change">Change Values</a>
+	
 	<?php endif; ?>
 </nav>
 
